172bc254898a2de80a3ca921cd8d06bffd288954
Will Rutland <wrutland07@gmail.com>
2024-06-15 17:16:32 +0100

    Setup base styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
