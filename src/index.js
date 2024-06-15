c64a21ef6f5c35443ddf5c61d5ddaadbef8726e5
Will Rutland <wrutland07@gmail.com>
2024-06-15 15:58:58 +0100

    Added vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics/>
   </React.StrictMode>
 );
