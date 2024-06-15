69444cd871c17852982350ef9a8e57e707826e2b
Will Rutland <wrutland07@gmail.com>
2024-06-15 16:03:03 +0100

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "verbose-debates.firebaseapp.com",
+  projectId: "verbose-debates",
+  storageBucket: "verbose-debates.appspot.com",
+  messagingSenderId: "1048499980348",
+  appId: "1:1048499980348:web:7557eefb07bb06373b5b39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
