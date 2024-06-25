d97e75deb6673c168e571000d50806bc260e8918
Will Rutland <wrutland07@gmail.com>
2024-06-25 09:30:13 +0100

    completed upvote and downvote system

--- src/components/pages/home/questionResponse.jsx
+++ src/components/pages/home/questionResponse.jsx
@@ -1,8 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {getFirestore, query, where, collection, getDocs} from 'firebase/firestore';
-import { Link } from 'react-router-dom';
+import {getFirestore, query, where, collection, getDocs, updateDoc, increment, doc, getDoc, arrayUnion} from 'firebase/firestore';
+import { useAuth } from '../../../context/authContext.jsx';
+import { Link, Navigate } from 'react-router-dom';
 
 export default function QuestionRespnse({postData, postersUserName}) {
+
+    const {auth, updateAuth} = useAuth();
+    console.log(auth)
+
+    const [greyedOut, setGreyedOut] = useState(false);
+    const [autoNav, setAutoNav] = useState(<></>);
     
     //get all data regarding the post
     const [profilePictureURL, setProfilePictureURL] = useState('');
@@ -12,10 +19,25 @@ export default function QuestionRespnse({postData, postersUserName}) {
     const [userReputation, setUserReputation] = useState(0);
     
     useEffect(() => {
-
         setUsername(postersUserName)
         setVotes(postData.votes);
         setUserPost(postData.post);
+        
+        //check if the currently logged in user has already interacted with this post
+        if (auth) {
+            const firestore = getFirestore();
+            const uid = auth.uid
+            const docRef = doc(firestore, 'questionResponses', postersUserName);
+            getDoc(docRef)
+            .then((doc) => {
+                const voters = doc.data().voters;
+                voters.forEach((voter) => {
+                    if (voter === uid) {
+                        setGreyedOut(true);
+                    };
+                });
+            });
+        };
         
         //now get the data about the user
         async function getUserData(username) {
@@ -25,6 +47,7 @@ export default function QuestionRespnse({postData, postersUserName}) {
                 const firestore = getFirestore();
                 const userData = query(collection(firestore, 'users'), where('username', '==', username));
                 const userDataSnap = await getDocs(userData);
+
 
                 //save the user data to a map and then return it
                 let res = {};
@@ -94,8 +117,9 @@ export default function QuestionRespnse({postData, postersUserName}) {
                                         <td>
                                             <button type="button" className="noVerticalSpacing" style={{width: '50%', padding: 0}} onClick={() => {
                                                 //upvote post
+                                                changeVotes(1);
                                             }}>
-                                                <h3 className="noVerticalSpacing">
+                                                <h3 className={`noVerticalSpacing ${greyedOut ? 'greyedOut' : ''}`} >
                                                     /\
                                                 </h3>
                                             </button>
@@ -110,8 +134,9 @@ export default function QuestionRespnse({postData, postersUserName}) {
                                         <td>
                                             <button type="button" className="noVerticalSpacing" style={{width: '50%', padding: 0}} onClick={() => {
                                                 //downvote post
+                                                changeVotes(-1);
                                             }}>
-                                                <h3 className="noVerticalSpacing">
+                                                <h3 className={`noVerticalSpacing ${greyedOut ? 'greyedOut' : ''}`}>
                                                     \/
                                                 </h3>
                                             </button>
@@ -123,6 +148,45 @@ export default function QuestionRespnse({postData, postersUserName}) {
                     </tr>
                 </thead>
             </table>
+            {autoNav}
         </div>
     );
+
+    async function changeVotes(ammount) {
+
+        //only run if the use is logged in
+        if (auth) {
+            const firestore = getFirestore();
+            const firestorePostRef = doc(firestore, 'questionResponses', username);
+    
+            //check if the user who voted has already voted on this post
+            const voters = await getDoc(firestorePostRef);
+            voters.data().voters.forEach((voter) => {
+                if (voter === auth.uid) {
+                    throw ('Already voted on this post');
+                };
+            });
+    
+            //increment the votes on the post
+            await updateDoc(firestorePostRef, {
+                votes: increment(ammount),
+            });
+    
+            //add the user to the list of voters
+            await updateDoc(firestorePostRef, {
+                voters: arrayUnion(auth.uid),
+            });
+    
+            //update local post votes
+            setVotes(votes + ammount);
+    
+            //grey out the vote buttons
+            setGreyedOut(true);
+        }
+        else {
+
+            //if the user is not logged in, take them to the account page
+            setAutoNav(<Navigate to="/account" />);
+        };
+    };
 };
