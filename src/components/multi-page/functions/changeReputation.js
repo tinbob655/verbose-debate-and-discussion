efdbbbda4588b5cc2909ce45d48eb52944280559
Will Rutland <wrutland07@gmail.com>
2024-06-30 12:28:23 +0100

    EVEN MORE bugfix

--- src/components/multi-page/functions/changeReputation.js
+++ src/components/multi-page/functions/changeReputation.js
@@ -9,7 +9,7 @@ export async function changeReputation(ammount, uid) {
         const userDocRef = doc(firestore, 'users', uid);
     
         await updateDoc(userDocRef, {
-            reputation: increment(ammount),
+            reputation: increment(1),
         });
     }
     else throw(`Can only change reputation by a value of +-${bound}`);
