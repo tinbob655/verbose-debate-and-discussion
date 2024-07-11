f67002c95558cb07192622de4c1880f00d5399db
Will Rutland <wrutland07@gmail.com>
2024-07-11 12:36:19 +0100

    added android and apple apps using capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.verbose.app',
+  appName: 'verbose',
+  webDir: 'build'
+};
+
+export default config;
